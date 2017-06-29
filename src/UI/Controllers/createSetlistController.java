5d8800ab92cce6ff38d8a01663a2b6decd897b30
Cooper <kertzc@live.com>
2017-06-28 22:28:27 -0500

    More work on setlist creation, working on its logic

--- src/UI/Controllers/createSetlistController.java
+++ src/UI/Controllers/createSetlistController.java
@@ -1,7 +1,50 @@
 package UI.Controllers;
 
+import DB.dbConnect;
+import Models.LibraryRecord;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Cooper on 6/28/2017.
  */
-public class createSetlistController {
+public class createSetlistController implements Initializable {
+    @FXML
+    private TableView<LibraryRecord> tableViewSetlist;
+    @FXML
+    private TableView<LibraryRecord> tableViewMusicLibrary;
+    @FXML
+    private TableColumn<LibraryRecord, String> colLibraryTitle;
+    @FXML
+    private TableColumn<LibraryRecord, String> colLibraryArtist;
+    @FXML
+    private TableColumn<LibraryRecord, String> colSetlistTitle;
+    @FXML
+    private TableColumn<LibraryRecord, String> colSetlistArtist;
+
+
+    @FXML
+    public void initialize(URL Location, ResourceBundle resources ){
+
+        //@TODO walk through mainmenu code and see why this would throw an error upon load. commenting it all out works fine
+        dbConnect dbLoad = new dbConnect();
+        ObservableList<LibraryRecord> data = FXCollections.observableArrayList();
+        try {
+            data = dbLoad.populateLibraryRecords();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        colLibraryTitle.setCellValueFactory(new PropertyValueFactory<>("Title"));
+        colLibraryArtist.setCellValueFactory(new PropertyValueFactory<>("Artist"));
+
+        tableViewMusicLibrary.setItems(data);
+    }
 }
